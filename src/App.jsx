75d9563342e634f667a51945eaaf0adce6469caa
Panharith <157610030+PanharithSor16@users.noreply.github.com>
2024-07-05 20:23:55 +0700

    code cofe template

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className=' bg-red-400 text-2xl'>
+      new class
+     </div>
+    </>
+  )
+}
+
+export default App
